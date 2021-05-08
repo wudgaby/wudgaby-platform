16c7bdc23477d44fda66c1f72a431660f812f8dc
wudgaby <wudgaby@sina.com>
2021-05-08 10:09:43 +0800

    init

--- maven install脚本.bat
+++ maven install脚本.bat
@@ -1,7 +1,1 @@
-mvn clean compile install -pl com.wudgaby.platform:basis-project -N
-mvn clean compile install -pl base-spring-boot-starters -N
-
-mvn clean package -pl basis-project-parent -N
-mvn clean compile install -pl basis-project-core -N
-mvn flatten:flatten basis-project -N
-mvn clean package -pl basis-project-parent -am -amd -Dmaven.test.skip=true
+mvn clean install -pl basis-project-web-core -N
