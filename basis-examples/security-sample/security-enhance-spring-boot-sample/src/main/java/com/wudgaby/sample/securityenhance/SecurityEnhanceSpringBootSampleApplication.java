f67b7d21b62d434ea94c4dac14a90acb003717af
wudgaby <wudgaby@qq.com>
2021-09-08 00:57:34 +0800

    整理

---
+++ basis-examples/security-sample/security-enhance-spring-boot-sample/src/main/java/com/wudgaby/sample/securityenhance/SecurityEnhanceSpringBootSampleApplication.java
@@ -0,0 +1,13 @@
+package com.wudgaby.sample.securityenhance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityEnhanceSpringBootSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityEnhanceSpringBootSampleApplication.class, args);
+    }
+
+}
---
+++ basis-examples/security-sample/security-enhance-spring-boot-sample/src/test/java/com/wudgaby/sample/securityenhance/SecurityEnhanceSpringBootSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wudgaby.sample.securityenhance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityEnhanceSpringBootSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
